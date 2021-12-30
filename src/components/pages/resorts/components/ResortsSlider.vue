ac1948170ed2b0bd19b9b76748fd3234786effd4
usman <usman.khalid444@gmail.com>
2021-12-30 10:17:03 -0800

    fixed

--- src/components/pages/resorts/components/ResortsSlider.vue
+++ src/components/pages/resorts/components/ResortsSlider.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <VueSlickCarousel :autoplay="true" :arrows="true" :dots="true">
+    <VueSlickCarousel
+      :autoplay="true"
+      :arrows="true"
+      :dots="true"
+      :lazyLoad="true"
+    >
       <div v-for="(img, i) in images" :key="i">
         <img
           class="w-full h-64 md:h-96 pointer-events-none"
